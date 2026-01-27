86f631bc0f081b0d5b96cb5c3ab3307dd15652a4
Harshasaisiddu143 <harsha.saripidakala@wmlit.com>
2026-01-27 18:17:04 +0530

    feat: Implemented Footer section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 export default function Page() {
   return (
     <>
       <Navbar />
+      <div className="py-40"></div>
+      <Footer />
     </>
   );
 }
