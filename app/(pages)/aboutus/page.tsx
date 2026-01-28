2462b2150a60f972c41d990f213b6a50c18fac6e
Harshasaisiddu143 <harsha.saripidakala@wmlit.com>
2026-01-28 14:17:02 +0530

    fix: Fix

--- app/(pages)/aboutus/page.tsx
+++ app/(pages)/aboutus/page.tsx
@@ -1,6 +1,6 @@
 import AboutUsPage from "@/app/components/AboutUs/About-us"
 import { Suspense } from "react"
-
+//page for about us
 export default function Aboutus() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
